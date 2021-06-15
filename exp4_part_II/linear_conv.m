63fc132dbc386daf904308dc4efa9ac1382aa516
HasanTurky <1706124@eee.buet.ac.bd>
2021-06-16 01:01:59 +0600

    matrix and dft method done

--- exp4_part_II/linear_conv.m
+++ exp4_part_II/linear_conv.m
@@ -20,9 +20,16 @@ for i = 1 : M
     A( : ,i ) = circshift2(x,i-1,N);
 end
 result_mat = A*h';
-subplot(211), stem(n,result_mat,'linewidth',3),title("result by matrix method")
+subplot(211), stem(n,result_mat,'linewidth',3),title("result by matrix method"),grid on, grid minor;
 
 %% using dft
 
+x_dft = zeros(1,length(x_pad));
+h_dft = zeros(1,length(h_pad));
+mtx = dftmtx(N);
+x_dft = x_pad*mtx;
+h_dft = h_pad*mtx;
 
-
+mult = x_dft.*h_dft;
+result_dft = ifft(mult,N);
+subplot(212), stem(n,result_dft,'linewidth',3),title("result by dft method"),grid on, grid minor;
